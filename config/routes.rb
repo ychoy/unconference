e9c01e80686bb5c9ceb97dfb7ee27b67a49d2701
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-08 12:09:55 -0700

    add API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :unconferences, defaults: {format: :json}
-  resources :talks, defaults: {format: :json}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	namespace :api do
+		resources :unconferences
+    resources :talks
+  end
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

de824fddbd0afe29eaec3ba82cca043c09e95231
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-08 09:12:39 -0700

    nesting talks into unconference routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :talks
-  resources :unconferences
+  resources :unconferences, defaults: {format: :json} do
+    resources :talks, defaults: {format: :json}
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

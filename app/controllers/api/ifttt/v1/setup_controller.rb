d651ca41fcfb9c9df6d0106e4172938ff8678128
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-08 14:59:12 -0700

    restructure setup function

--- app/controllers/api/ifttt/v1/setup_controller.rb
+++ app/controllers/api/ifttt/v1/setup_controller.rb
@@ -2,15 +2,15 @@ class Api::Ifttt::V1::SetupController < ActionController
 	before_action :return_errors_unless_valid_channel_key
 
   def setup
-  	render json: {
-			data = {
-      	samples: {
- 					triggers: {
-          	created_new_talk: { }
-        	}
-      	}
-    	}
+    data = {
+      samples: {
+        triggers: {
+          created_new_talk: { }
+        }
+      }
     }
+
+  	render json: data
   end
 
   private

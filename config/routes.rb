d8bba865048bb12d1519bd02dfddbaea359d70e3
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-08 09:14:28 -0700

    remove nested structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :unconferences, defaults: {format: :json} do
-    resources :talks, defaults: {format: :json}
-  end
+  resources :unconferences, defaults: {format: :json}
+  resources :talks, defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

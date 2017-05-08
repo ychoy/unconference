4bbfddb1158c4f2e68f222a7ed962172d2145e52
Yan Yin Choy <yanyinchoy@gmail.com>
2017-05-08 14:48:55 -0700

    add ifttt routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 		resources :unconferences
     resources :talks
   end
+
+  get  "/api/ifttt/v1/status", to: "ifttt#status"
+  post "/api/ifttt/v1/test/setup", to: "ifttt#setup"
+
+  post "/api/ifttt/v1/triggers/new_talk_created", to: "ifttt#new_talk_created"
+  post "/api/ifttt/v1/actions/created_new_talk", to: "ifttt#create_new_talk"
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
